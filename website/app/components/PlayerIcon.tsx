c9fc9d4b0f00842935b8d46ef8a4f39545af757d
Jack Arthur Harrhy <me@jackharrhy.com>
2025-11-02 15:45:59 -0330

    music note chem

--- website/app/components/PlayerIcon.tsx
+++ website/app/components/PlayerIcon.tsx
@@ -20,17 +20,27 @@ export function PlayerIcon({
   };
 
   return (
-    <img
-      src={imageUrl}
-      alt={alt}
-      className={cn(
-        sizeClasses[size],
-        "object-fit drop-shadow-sm",
-        player === null && "filter brightness-0 opacity-20"
-      )}
-      onMouseOver={(event)=>{document.body.dataset.player=player?.name}}
-      onMouseLeave={(event)=>{delete document.body.dataset.player}}
+    <div
+      className={cn("relative", sizeClasses[size])}
       data-player={player?.name}
-    />
+    >
+      <img
+        src={imageUrl}
+        alt={alt}
+        className={cn(
+          "object-fit drop-shadow-sm h-full w-full",
+          player === null && "filter brightness-0 opacity-20"
+        )}
+        onMouseOver={(event) => {
+          document.body.dataset.player = player?.name;
+        }}
+        onMouseLeave={(event) => {
+          delete document.body.dataset.player;
+        }}
+      />
+      <span className="opacity-0 transition-opacity duration-200 absolute right-[-0.5rem] bottom-[-0.7rem] text-pink-300 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-[1.4em] font-bold pointer-events-none select-none animate-[wiggle_0.5s_infinite]">
+        ♪
+      </span>
+    </div>
   );
 }
